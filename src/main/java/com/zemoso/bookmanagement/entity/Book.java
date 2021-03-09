92dc1b6ff92e832740a425e0c9f3c22a9708107f
VaidikNissangi9 <vaidik.nissangi@zemosolabs.com>
2021-03-09 09:28:15 +0530

    initial scaffolding

--- src/main/java/com/zemoso/bookmanagement/entity/Book.java
+++ src/main/java/com/zemoso/bookmanagement/entity/Book.java
@@ -1,19 +1,5 @@
 package com.zemoso.bookmanagement.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class Book {
 
-    public String id;
-
-    public String name;
-
-    public String category;
-
-    public String description;
-
-    public String author;
 }
